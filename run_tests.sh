648054e70983ad23be3f5998dfd5684afda5350e
root <root@ISI-BIGCLUSTER-PNRR10.maas>
2025-09-15 08:46:02 +0200

    feat: push single machine dtgraph evaluation results

--- run_tests.sh
+++ run_tests.sh
@@ -12,6 +12,6 @@ echo "Starting STGraph container..."
 docker compose -f docker-compose_dtgraph.yaml up
 echo "Evaluation of STGraph completed, results available in the results/ directory."
 
-echo "Running AeonG evaluation experiments..."
-docker compose -f docker-compose_aeong.yaml up
-echo "Evaluation of AeonG completed, results available in the results/ directory."
+#echo "Running AeonG evaluation experiments..."
+#docker compose -f docker-compose_aeong.yaml up
+#echo "Evaluation of AeonG completed, results available in the results/ directory."
